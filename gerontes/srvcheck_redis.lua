8f0a94b4495626c9074e2be774c93b26a7ecddbc
Mihai Craiu <mihai.craiu@1and1.ro>
2026-02-24 16:28:13 +0200

    redis auth

--- gerontes/srvcheck_redis.lua
+++ gerontes/srvcheck_redis.lua
@@ -2,6 +2,9 @@ local function worker(label, ip, port, opt)
     return pcall(function()
         local redis = require('redis')
         redis = redis.connect(ip, port)
+        if opt.redisAuth then
+            redis.auth(opt.redisAuth)
+        end
         local info = redis:info('server').server
         redis = nil
         return 1000 * (math.floor(info.server_time_usec/1000000) - info.uptime_in_seconds)
